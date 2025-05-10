ae7c17d4844834f3f4b29cb8a6354f806e9bc563
xy7-ux <ostrowskiszkola8@gmail.com>
2025-05-10 13:58:59 +0200

    Initial Commit

---
+++ Patryk_Ostrowski_iOS_Wrocław/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Patryk_Ostrowski_iOS_Wrocław
+//
+//  Created by Patryk Ostrowski on 10/05/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Patryk_Ostrowski_iOS_Wrocław/Patryk_Ostrowski_iOS_Wroc_awApp.swift
@@ -0,0 +1,17 @@
+//
+//  Patryk_Ostrowski_iOS_Wroc_awApp.swift
+//  Patryk_Ostrowski_iOS_Wrocław
+//
+//  Created by Patryk Ostrowski on 10/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct Patryk_Ostrowski_iOS_Wroc_awApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
